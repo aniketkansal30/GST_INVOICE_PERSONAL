04e6c844b29e253a5924e9d9811369e1dec11e8a
aniketkansal30 <aniketkansal3007@gmail.com>
2026-05-05 16:57:12 +0530

    fix: hardcode cors origins

--- gst-invoice-app/server/index.js
+++ gst-invoice-app/server/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({ 
-  origin: ['http://localhost:3000', 'https://gst-invoice-personal.vercel.app/'],
+  origin: ['http://localhost:3000', 'https://gst-invoice-personal.vercel.app'],
   credentials: true 
 }));
 app.use(express.json());
