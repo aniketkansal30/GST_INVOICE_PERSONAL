21af2e7fd3683602e2f4f702820a011e00a49a90
aniketkansal30 <aniketkansal3007@gmail.com>
2026-05-09 12:59:50 +0530

    update

--- gst-invoice-app/server/index.js
+++ gst-invoice-app/server/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({ 
-  origin: ['http://localhost:3000', 'https://gst-invoice-personal.vercel.app'],
+  origin: ['http://localhost:3000', 'https://gst-invoice-personal.vercel.app', 'null'],
   credentials: true 
 }));
 app.use(express.json());
